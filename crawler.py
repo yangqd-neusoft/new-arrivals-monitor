a7e06a8b3c529e2c6a52e638f44cfd61fe47fabe
Yangqd <yangqdfight@163.com>
2017-12-04 16:34:40 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,1 +1,14 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
+URL = "http://kq.neusoft.com/"
+
+browser=webdriver.PhantomJS(executable_path='D:\\Python27\phantomjs-2.1.1-windows\\bin\\phantomjs.exe')
+browser.get(URL)
+time.sleep(3)
+
+browser.quit()
